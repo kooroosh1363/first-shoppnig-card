81d89bff480def871ba19b9de79c1489667ba8eb
kooroosh1363 <peymanraad1363@gmail.com>
2023-06-17 23:32:09 +0400

    add new file and codes

---
+++ header.php
@@ -0,0 +1,12 @@
+<header>
+    <img src="#" alt="img">
+    <div class="txt">
+        <h1>SHOPPING <br> CLOTHS</h1>
+    </div>
+    <nav>
+        <a href="index.php"><img src="#" class="img-logo" alt="logo"></a>
+        <div>
+            <a href="Mycard.php"><i class="box box-card-down"></i><sup>0</sup></a>
+        </div>
+    </nav>
+</header>
